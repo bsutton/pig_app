8040e1c38afed5a349f11ed5351787fc37692163
Brett Sutton <bsutton@noojee.com.au>
2025-01-25 09:53:53 +1100

    fixed the settings so that it works with en env var at runtime  so we can change the url when debugging.

--- lib/api/settings.dart
+++ lib/api/settings.dart
@@ -1,7 +1,8 @@
+import 'dart:io';
+
 const debugServerUrl = 'http://localhost:1080';
 
 const releaseServerUrl = 'http://irrigation';
 
 // ignore: do_not_use_environment
-String serverUrl = const String.fromEnvironment('SERVER_URL',
-    defaultValue: 'http://irrigation');
+String serverUrl = Platform.environment['SERVER_URL'] ?? 'http://irrigation';
