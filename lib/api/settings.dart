de888c96685cea9df935827e1f30610449c185fe
Brett Sutton <bsutton@noojee.com.au>
2025-05-31 10:09:38 +1000

    moved back to usig an asset/.env file to configure urls.

--- lib/api/settings.dart
+++ lib/api/settings.dart
@@ -1,29 +1,7 @@
-import 'dart:js_interop' as js;
-
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-String get releaseServerUrl => origin; // 'http://irrigation';
-
 // ignore: do_not_use_environment
-String serverUrl = dotenv.env['SERVER_URL'] ?? releaseServerUrl;
+String serverUrl =
+    dotenv.env['SERVER_URL'] ?? 'http://localhost:1080'; 
 
-String webSocketUrl = dotenv.env['WS_SERVER_URL'] ?? wsSocketUrl;
-
-String get origin => getOrigin();
-
-String get wsSocketUrl {
-  // Get the current server's origin using JS interop
-  final origin = getOrigin();
-
-  // Determine if the origin is using https or http and adjust the WebSocket protocol
-  final wsProtocol = origin.startsWith('https') ? 'wss' : 'ws';
-
-  // Replace the protocol and append the WebSocket port or endpoint
-  final wsUrl = '${origin.replaceFirst(RegExp('^https?'), wsProtocol)}:1080';
-
-  return wsUrl;
-}
-
-/// JS interop function to access `window.location.origin`
-@js.JS()
-external String getOrigin();
+String webSocketUrl = dotenv.env['WS_SERVER_URL'] ?? 'ws://localhost:1080';
