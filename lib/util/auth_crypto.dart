b9dcab15e372183309564f9f2f350ce38253dbeb
Brett Sutton <bsutton@noojee.com.au>
2025-12-28 11:46:32 +1100

    on browser now use a crypto which can run in a web work for better ui responsiveness.

--- lib/util/auth_crypto.dart
+++ lib/util/auth_crypto.dart
@@ -1,26 +1,35 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:cryptography/cryptography.dart' as crypto;
 import 'package:pointycastle/export.dart';
 
 class AuthCrypto {
-  static Uint8List deriveKey({
+  static Future<Uint8List> deriveKey({
     required String password,
     required String saltBase64,
     required List<int> params,
     required String algorithm,
-  }) {
+  }) async {
     if (params.length < 3) {
       throw ArgumentError('PBKDF2 params must include 3 values.');
     }
-    final blockLength = params[0];
+    // final blockLength = params[0];
     final iterationCount = params[1];
     final desiredKeyLength = params[2];
     final salt = base64.decode(saltBase64);
-    final digest = _digestForAlgorithm(algorithm);
-    final pbkdf2 = PBKDF2KeyDerivator(HMac(digest, blockLength))
-      ..init(Pbkdf2Parameters(salt, iterationCount, desiredKeyLength));
-    return pbkdf2.process(Uint8List.fromList(utf8.encode(password)));
+    final macAlgorithm = _macForAlgorithm(algorithm);
+    final pbkdf2 = crypto.Pbkdf2(
+      macAlgorithm: macAlgorithm,
+      iterations: iterationCount,
+      bits: desiredKeyLength * 8,
+    );
+    final secretKey = await pbkdf2.deriveKey(
+      secretKey: crypto.SecretKey(utf8.encode(password)),
+      nonce: salt,
+    );
+    final bytes = await secretKey.extractBytes();
+    return Uint8List.fromList(bytes);
   }
 
   static String hmacHex(Uint8List key, String message) {
@@ -29,12 +38,12 @@ class AuthCrypto {
     return _bytesToHex(digest);
   }
 
-  static Digest _digestForAlgorithm(String algorithm) {
+  static crypto.MacAlgorithm _macForAlgorithm(String algorithm) {
     switch (algorithm) {
       case 'pbkdf2-sha256':
-        return SHA256Digest();
+        return crypto.Hmac.sha256();
       case 'pcks':
-        return SHA512Digest();
+        return crypto.Hmac.sha512();
     }
     throw ArgumentError('Unsupported auth algorithm: $algorithm');
   }
